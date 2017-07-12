151b2b247a98edab03a37d227772cb4856ae8aab
zexumath <number1.xuze@gmail.com>
2017-07-11 18:26:08 -0700

    to write main function into a class

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pygame
+import random
+import os
+from pygame.locals import *
+from src import *
+
+class RiichiMahjong:
+
+    windowWidth  = 800
+    windowHeight = 600
+
