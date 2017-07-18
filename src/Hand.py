bc9117607ca067a8f9b2510b0bab105e8ea620ae
dagude765 <xuzhitao0570@163.com>
2017-07-18 21:19:49 +0800

    Change -48 to -49 due to my mistake. Merge Hand class into GameTable class.

--- src/Hand.py
+++ src/Hand.py
@@ -12,12 +12,12 @@ class Hand:
 
     def new_set_init(self, yama, position, oya_position):
         if position == oya_position:
-            self.in_hand = yama[-4:] + yama[-20:-16] + yama[-36:-32] + [yama[-48]]
+            self.in_hand = yama[-4:] + yama[-20:-16] + yama[-36:-32] + [yama[-49]]
         else:
             position_to_oya = (position - oya_position) % NUM_OF_SET_PER_QUAN
             pais_to_pick = [yama[-(position_to_oya + j * 4 + 1) * 4:-(position_to_oya + j * 4) * 4] for j in range(3)]
             self.in_hand = [j for i in pais_to_pick for j in i]
-            self.in_hand.append(yama[-48 - position_to_oya])
+            self.in_hand.append(yama[-49 - position_to_oya])
 
         self.in_hand.sort()
         self.new_pai = []
